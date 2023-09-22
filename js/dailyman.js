5c03c1b1844862e2d9d48cdb92e95cee17e92483
MD Tanvirul Islam <57485202+dhtanvir@users.noreply.github.com>
2023-09-22 19:24:42 +0600

    Add files via upload

---
+++ js/dailyman.js
@@ -0,0 +1,26 @@
+
+/* === swiper slide Start== */
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  spaceBetween: 50,
+  slidesPerGroup: 2,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+/* === swiper slide End== */
+
+
+
+
+
+
+
